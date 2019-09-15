f56ac4f0020f21e76f218326d38858fac954c35e
Callum <callmc61@gmail.com>
2019-09-15 13:00:15 +1000

    Got a graph to plot! Cleaned up script

--- MXB103 Group Project/Group_13_Project_Script.m
+++ MXB103 Group Project/Group_13_Project_Script.m
@@ -1,6 +1,6 @@
 %Group-13 Project Script
 
-%Initiate values for Euler Method
+%% Initiate values for Euler Method
 H = 74;
 D = 31;
 c = 0.9;
@@ -8,21 +8,30 @@ m = 80;
 L = 25;
 k = 90;
 g = 9.8;
-T = 100;
-n = 100;
+T = 60;
+n = 60;
 
-%Initiate C and K
+%% Initiate C and K
 C = c/m;
 K = k/m;
 
-%Run Euler function
-[t_euler, y_euler, v_euler, h_euler] = euler_bungee(T, n, g, C, K, L)
+%% Run Euler function
+[t, y, v, h] = euler_bungee(T, n, g, C, K, L)
 
-%Initiate variables for Taylor2 function
+%% Initiate variables for Taylor2 function
 f = @(v, t) g - C*abs(v(end))*v(end) - max(0, K*(y(end) - L));
 fdash = @(v, t) diff(g - C*abs(v(end))*v(end) - max(0, K*(y(end) - L)));
 a = 0;
 b = T;
 alpha = y(a+1);
-%Run Second Order Taylor Function
-[t_taylor, w_taylor, h_taylor] = modeuler_bungee(f, a, b, alpha, n)
+
+%% Run Second Order Taylor Function
+[t_modeuler, w_modeuler, h_moduler] = modeuler_bungee(f, a, b, alpha, n)
+
+%% Plot modeuler function to display jump
+
+figure();
+plot(t_modeuler, w_modeuler);
+title('f(t,w) of modeuler function for Jump')
+xlabel('Time (t)')
+ylabel('Position in relation to Jump (w)')
