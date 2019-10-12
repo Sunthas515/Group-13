4b4a15cb558600ca5b42b7629cab8e44a19f99b7
dontaskmemyname <55371333+dontaskmemyname@users.noreply.github.com>
2019-10-12 12:23:43 +1000

    Fixed Integration section

--- MXB103 Group Project/trapezoidal_integration.m
+++ MXB103 Group Project/trapezoidal_integration.m
@@ -1,18 +1,20 @@
-function integration = trapezoidal_integration(f, n)
+function integral = trapezoidal_integration(f,h)
 % Takes in a function and returns the integral of it given that
 % f = f(x)
 % n = number of subintervals (in this case seconds)
 % f(0) = a
 % f(end) = b
-% h = (b-a)/n
+% h = (b-a)/n;
+% x = a:h:b;
+n = length(f);
 sum = 0;
 
 % Sum middle terms
-for i=2:length(f)-1
+for i = 2:n
     sum = sum + f(i);
 end
 
 % Calculate final definite integral
-h = (f(end) - f(1))/n;
-integration = h/2 * (f(1) + 2*sum + f(end));
+% h = (f(n) - f(1))/n;
+integral = h/2 * (f(1) + 2*sum + f(n));
 end
